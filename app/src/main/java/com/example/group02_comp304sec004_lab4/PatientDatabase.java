1396f8578a2f05eb4ab623cb94561f78f64f5213
@mae-lab-hub <asmaeallou@outlook.com>
2022-11-20 23:13:13 -0500

    Added PatientDatabase. Created empty TestDatabase since the PatientDatabase needed it. TO DO: Create the files for Test data. App runs fine

---
+++ app/src/main/java/com/example/group02_comp304sec004_lab4/PatientDatabase.java
@@ -0,0 +1,54 @@
+package com.example.group02_comp304sec004_lab4;
+
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Patient.class}, version = 1, exportSchema = false)
+public abstract class PatientDatabase extends RoomDatabase {
+
+    public abstract PatientDao patientDao();
+
+    private static volatile PatientDatabase INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    static PatientDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (TestDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    PatientDatabase.class, "patient_database")
+                            .addCallback(sRoomDatabaseCallback)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+
+            // If you want to keep data through app restarts,
+            // comment out the following block
+            databaseWriteExecutor.execute(() -> {
+                // Populate the database in the background.
+                // If you want to start with more patients, just add them.
+                PatientDao dao = INSTANCE.patientDao();
+                dao.deleteAll();
+            });
+        }
+    };
+}
---
+++ app/src/main/java/com/example/group02_comp304sec004_lab4/TestDatabase.java
@@ -0,0 +1,4 @@
+package com.example.group02_comp304sec004_lab4;
+
+public class TestDatabase {
+}
