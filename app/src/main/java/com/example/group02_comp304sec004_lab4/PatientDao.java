17bc0b8f800306bcfc682f1a96e4e726d2dc626f
@mae-lab-hub <asmaeallou@outlook.com>
2022-11-20 22:58:03 -0500

    Added Patient class and PatientDao. App Runs fine

---
+++ app/src/main/java/com/example/group02_comp304sec004_lab4/PatientDao.java
@@ -0,0 +1,33 @@
+package com.example.group02_comp304sec004_lab4;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface PatientDao {
+
+    @Insert
+    void insert(Patient patient);
+
+    @Update
+    void update(Patient patient);
+
+    @Delete
+    void delete(Patient patient);
+
+    @Query("DELETE FROM patient_table")
+    void deleteAll();
+
+    @Query("Select * FROM patient_table where patientID = :patientID")
+    LiveData<Patient> getByPatientID(int patientID);
+
+    @Query("Select * FROM patient_table")
+    LiveData<List<Patient>> getAllPatients();
+
+}
